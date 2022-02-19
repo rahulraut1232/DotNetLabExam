91b0c28416f35d71000090f3a88c99152e0d2db6
rahulraut1232 <91686715+rahulraut1232@users.noreply.github.com>
2022-02-19 19:17:50 +0530

    Add files via upload

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,138 @@
+﻿using MVCExamMain.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCExamMain.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Index()
+        {
+            List<Products> list = new List<Products>();
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = @"Data Source=(localdb)\ProjectsV13;Initial Catalog=JKJan22;Integrated Security=True;Connect Timeout=30;";
+            cn.Open();
+
+            SqlCommand cmdPdk = new SqlCommand();
+            cmdPdk.CommandType = System.Data.CommandType.Text;
+            cmdPdk.CommandText = "select * from Products";
+            try
+            {
+                SqlDataReader dr = cmdPdk.ExecuteReader();
+                while (dr.Read())
+                {
+                    list.Add(new Products
+                    {
+                        ProductId = (int)dr["ProductId"],
+                        ProductName = (string)dr["ProductName"],
+                        Rate = (decimal)dr["Rate"],
+                        Description = (string)dr["Description"],
+                        CategoryName = (string)dr["CategoryName"]
+                    });
+                }
+            }
+            catch (Exception exe)
+            {
+                Console.WriteLine(exe.Message);
+            }
+            finally {
+                cn.Close();
+            }
+            return View(list);
+        }
+
+        // GET: Products/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Products/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Products/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Products/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Products/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, Products obj)
+        {
+            SqlConnection cn = new SqlConnection();
+            cn.ConnectionString = @"Data Source=(localdb)\ProjectsV13;Initial Catalog=JKJan22;Integrated Security=True;Connect Timeout=30;";
+            cn.Open();
+
+            SqlCommand cmdPdk = new SqlCommand();
+            cmdPdk.CommandType = System.Data.CommandType.Text;
+            cmdPdk.CommandText = "update Products set ProductName = @ProductName,Rate = @Rate,Description = @Description,CategoryName = @CategoryName where ProductId = @ProductId";
+
+            cmdPdk.Parameters.AddWithValue("ProductName",obj.ProductName);
+            cmdPdk.Parameters.AddWithValue("Rate", obj.Rate);
+            cmdPdk.Parameters.AddWithValue("Description", obj.Description);
+            cmdPdk.Parameters.AddWithValue("CategoryName", obj.CategoryName);
+            cmdPdk.Parameters.AddWithValue("ProductId", id);
+
+            try
+            {
+                cmdPdk.ExecuteNonQuery();
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return View();
+            }
+            finally {
+                cn.Close();
+            }
+        }
+
+        // GET: Products/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Products/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Models/Products.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCExamMain.Models
+{
+    public class Products
+    {
+        [Key]
+        [Display(Name = "Product Id")]
+        public int ProductId { get; set; }
+        
+        [DataType(DataType.Text)]
+        [Required(ErrorMessage ="Please Enter Product Name")]
+        [Display(Name = "Product Name")]
+        [StringLength(10,ErrorMessage ="Given {0} Value Shoud not exceed {1} characters. ")]
+        public string ProductName { get; set; }
+
+        [Range(2000,60000,ErrorMessage ="Please enter value between 2000-60000")]
+        [MaxLength(6),MinLength(4)]
+        [Display(Name ="Rate")]
+        [DataType(DataType.Currency)]
+        public decimal Rate { get; set; }
+
+        [DataType(DataType.Text)]
+        [Required(ErrorMessage = "Please Enter Description")]
+        [StringLength(10, ErrorMessage = "Given {0} Value Shoud not exceed {1} characters. ")]
+        public string Description { get; set; }
+
+        [DataType(DataType.Text)]
+        [Required(ErrorMessage = "Please Enter Category")]
+        [StringLength(10, ErrorMessage = "Given {0} Value Shoud not exceed {1} characters. ")]
+        [Display(Name = "Category Name")]
+        public string CategoryName { get; set; }
+    }
+}
